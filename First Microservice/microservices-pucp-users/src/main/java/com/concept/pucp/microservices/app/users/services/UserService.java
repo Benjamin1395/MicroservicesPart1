3abd401ecd090b95764a8b38f57d832f7a62b80c
Benjamin1395 <52389293+Benjamin1395@users.noreply.github.com>
2023-04-14 15:38:59 -0500

    Add files via upload

---
+++ First Microservice/microservices-pucp-users/src/main/java/com/concept/pucp/microservices/app/users/services/UserService.java
@@ -0,0 +1,13 @@
+package com.concept.pucp.microservices.app.users.services;
+
+import java.util.Optional;
+
+import com.concept.pucp.microservices.app.users.models.entity.User;
+
+public interface UserService {
+	
+	public Iterable<User> findAll();
+	public Optional <User> findById(Long id);
+	public User save (User user);
+	public void deleteById (Long id);
+}
